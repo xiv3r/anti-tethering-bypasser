1542fa057977614005e69ad42760a61dc9d6f740
x!v3r <117867334+xiv3r@users.noreply.github.com>
2024-09-25 11:35:44 +0800

    Update persistent.sh

--- persistent.sh
+++ persistent.sh
@@ -2,6 +2,6 @@
 
 # Run after Boot
 opkg install wget iptables-mod-ipopt iptables-zz-legacy -y
-cd /etc/init.d/
-wget -O antitethering https://raw.githubusercontent.com/xiv3r/anti-tethering-bypasser/refs/heads/main/anti-tethering.sh
-chmod +x antitehering
+cd /etc/
+wget -O rc.local https://raw.githubusercontent.com/xiv3r/anti-tethering-bypasser/refs/heads/main/anti-tethering.sh
+chmod +x rc.local
