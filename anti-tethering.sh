308d1fc43770eed83cddb7e06f857db13021602c
x!v3r <117867334+xiv3r@users.noreply.github.com>
2024-09-25 10:51:54 +0800

    Update anti-tethering.sh

--- anti-tethering.sh
+++ anti-tethering.sh
@@ -11,7 +11,9 @@ WAN="wlan0"
 iptables -F
 iptables -t nat -F -t mangle -F
 # Apply Routing
+iptables -t mangle -I POSTROUTING -o $WAN -j ttl --ttl-set 64
+iptables -t mangle -I POSTROUTING -o $WAN -m ttl ! --ttl-eq 255 -j ttl --ttl-set 64
 iptables -t nat -A POSTROUTING -o $WAN -j MASQUERADE
-iptables -t mangle -A PREROUTING -j TTL --ttl-set 65
+iptables -t mangle -A PREROUTING -j ttl --ttl-set 64
 iptables -A FORWARD -i $WAN -o $LAN -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i $LAN -o $WAN -j ACCEPT
