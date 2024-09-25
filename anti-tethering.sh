bb9778d0d415b28b3255e3c7a3fc2f2915168525
x!v3r <117867334+xiv3r@users.noreply.github.com>
2024-09-25 08:51:36 +0800

    Create anti-tethering.sh

---
+++ anti-tethering.sh
@@ -0,0 +1,12 @@
+#!/bin/sh /etc/init.d/
+
+LAN="eth0"
+WAN="wlan0"
+# Flush Tables
+iptables -F
+iptables -t nat -F -t mangle -F
+# Apply Routing
+iptables -t nat -A POSTROUTING -o $WAN -j MASQUERADE
+iptables -t mangle -A PREROUTING -j TTL --ttl-set 65
+iptables -A FORWARD -i $WAN -o $LAN -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i $LAN -o $WAN -j ACCEPT
