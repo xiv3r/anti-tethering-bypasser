67b0d55d51a035866e9b14775ddf6765893ae03c
x!v3r <117867334+xiv3r@users.noreply.github.com>
2024-09-25 13:20:02 +0800

    Update anti-tethering.sh

--- anti-tethering.sh
+++ anti-tethering.sh
@@ -2,7 +2,6 @@
 
 # Setup
 # WISP 10.0.0.1 TTL/HL=1 -> OpenWRT w/ bypassed -> 10.0.0.1 TTL/HL=64
-
 # Interfaces 
 LAN="eth0"
 WAN="wlan0"
@@ -13,10 +12,9 @@ iptables -t nat -F -t mangle -F
 
 # Apply Routing
 iptables -t mangle -I POSTROUTING -o $WAN -j TTL --ttl-set 64
-iptables -t mangle -I POSTROUTING -o $WAN -m ttl ! --ttl-eq 255 -j TTL --ttl-set 64
 iptables -t nat -A POSTROUTING -o $WAN -j MASQUERADE
 iptables -t mangle -A PREROUTING -j TTL --ttl-set 64
 iptables -A FORWARD -i $WAN -o $LAN -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables  i-A FORWARD -i $LAN -o $WAN -j ACCEPT
+iptables i-A FORWARD -i $LAN -o $WAN -j ACCEPT
 
 exit 0
