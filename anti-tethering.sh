deb6b56f66a5a267b930fe7e52c4f2e2eeed21aa
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-09-30 19:24:38 +0800

    Update anti-tethering.sh

--- anti-tethering.sh
+++ anti-tethering.sh
@@ -3,13 +3,13 @@
 # WISP 10.0.0.1 TTL/HL=1 -> OpenWRT w/ bypassed -> LAN/WLAN=>10.0.0.1 TTL/HL=64
 
 # Flush mangle table rules
-iptables -t mangle -F
+iptables -F
 
-# Input TTL=1 and Output TTL=64
+# Append ttl before prerouting (TTL=1 and Output TTL=64)
 iptables -t mangle -A PREROUTING -i wlan0 -j TTL --ttl-set 64
 
 # Redirect all traffic from wlan0 to eth0
 iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
-iptables -A FORWARD -i eth0 -o wlan0 -j ACCEPT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i eth0 -o wlan0 -j ACCEPT
 
 exit 0
