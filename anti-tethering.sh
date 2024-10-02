1bfbeaf49177b3028f084b205432bf336cda1edf
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-02 15:42:57 +0800

    Update anti-tethering.sh

--- anti-tethering.sh
+++ anti-tethering.sh
@@ -1,17 +1,20 @@
 #!/bin/sh /etc/rc.local
 
 # WISP 10.0.0.1 TTL/HL=1 -> OpenWRT w/ bypassed -> LAN/WLAN=>10.0.0.1 TTL/HL=64
-
-# Flush mangle table rules
 iptables -F
 iptables -t mangle -F
-
-# Append ttl in both  prerouting and postrouting (TTL=1 and Output TTL=64)
-iptables -t mangle -A POSTROUTING -o wlan0 -j TTL --ttl-set 64
 iptables -t mangle -A PREROUTING -i wlan0 -j TTL --ttl-set 64
+iptables -t mangle -A POSTROUTING -o wlan0 -j TTL --ttl-set 64
+iptables -A FORWARD -i wlan0 -o br-lan -j ACCEPT
+iptables -A FORWARD -i br-lan -o wlan0 -j ACCEPT
+iptables -P FORWARD ACCEPT
 
-# Redirect all traffic from wlan0 to eth0
-iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
-iptables -A FORWARD -i eth0 -o wlan0 -j ACCEPT
+ip6tables -F
+ip6tables -t mangle -F
+ip6tables -t mangle -A PREROUTING -i wlan0 -j HL --hl-set 64
+ip6tables -t mangle -A POSTROUTING -o wlan0 -j HL --hl-set 64
+ip6tables -A FORWARD -i wlan0 -o br-lan -j ACCEPT
+ip6tables -A FORWARD -i br-lan -o br-lan -j ACCEPT
+ip6tables -P FORWARD ACCEPT
 
 exit 0
