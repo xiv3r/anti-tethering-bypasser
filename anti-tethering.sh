d49b6b2299beb8683e9fbe2c6221b587137734e9
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-03 20:32:30 +0800

    Update anti-tethering.sh

--- anti-tethering.sh
+++ anti-tethering.sh
@@ -1,3 +1,4 @@
+echo " Downloading Dependencies"
 opkg install iptables-mod-ipopt kmod-ipt-ipopt kmod-ipt-nat iptables-zz-legacy iptables ip6tables ip6tables-zz-legacy ip6tables-mod-nat kmod-ipt-nat6 kmod-ip6tables kmod-zram
 
 echo "Installing iptables rule to /etc/rc.local"
@@ -12,7 +13,7 @@ echo "iptables -A FORWARD -i wlan0 -o br-lan -j ACCEPT" >> /etc/rc.local
 echo "iptables -A FORWARD -i br-lan -o wlan0 -j ACCEPT" >> /etc/rc.local
 echo "iptables -P FORWARD ACCEPT" >> /etc/rc.local
 
-echo"Installing Ip6tables rule to /etc/rc.local"
+echo"Installing ip6tables rule to /etc/rc.local"
 echo "ip6tables -F" >> /etc/rc.local
 echo "ip6tables -t mangle -F" >> /etc/rc.local
 echo "ip6tables -t mangle -A PREROUTING -i wlan0 -j HL --hl-set 64" >> /etc/rc.local
@@ -23,4 +24,5 @@ echo "ip6tables -P FORWARD ACCEPT" >> /etc/rc.local
 
 echo "exit 0" >> /etc/rc.local
 chmod +x /etc/rc.local
-echo "Done Installing into /etc/rc.local"
+echo "Done Installing iptables and ip6tables rule into /etc/rc.local"
+echo "Reboot the router to apply the setting"
