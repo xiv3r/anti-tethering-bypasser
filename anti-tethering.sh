6e9258a6b5a30c1fd259b79914904e4ab25af988
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-03 12:08:27 +0800

    Update anti-tethering.sh

--- anti-tethering.sh
+++ anti-tethering.sh
@@ -1,5 +1,6 @@
 opkg install wget iptables-mod-ipopt kmod-ipt-ipopt kmod-ipt-nat iptables-zz-legacy iptables ip6tables ip6tables-zz-legacy ip6tables-mod-nat kmod-ipt-nat6 kmod-ip6tables kmod-zram
 
+echo "" > /etc/rc.local
 echo "#!/bin/sh /etc/rc.local" >> /etc/rc.local
 echo "# WISP 10.0.0.1 TTL/HL=1 -> OpenWRT w/ bypassed -> LAN/WLAN=>10.0.0.1 TTL/HL=64" >> /etc/rc.local
 echo "iptables -F" >> /etc/rc.local
