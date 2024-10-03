fee60d38fe9a9bc306eb9135fbb7ff6ca85bcd3d
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-03 20:40:44 +0800

    Update anti-tethering.sh

--- anti-tethering.sh
+++ anti-tethering.sh
@@ -2,8 +2,7 @@ echo " Downloading Dependencies"
 opkg install iptables-mod-ipopt kmod-ipt-ipopt kmod-ipt-nat iptables-zz-legacy iptables ip6tables ip6tables-zz-legacy ip6tables-mod-nat kmod-ipt-nat6 kmod-ip6tables kmod-zram
 
 echo "Installing iptables rule to /etc/rc.local"
-echo "" > /etc/rc.local
-echo "#!/bin/sh /etc/rc.local" >> /etc/rc.local
+echo "#!/bin/sh /etc/rc.local" > /etc/rc.local
 echo "# WISP 10.0.0.1 TTL/HL=1 -> OpenWRT w/ bypassed -> LAN/WLAN=>10.0.0.1 TTL/HL=64" >> /etc/rc.local
 echo "iptables -F" >> /etc/rc.local
 echo "iptables -t mangle -F" >> /etc/rc.local
