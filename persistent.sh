c5acd3c6baa87aa7c222292d9f76a2edb827948a
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-03 08:54:33 +0800

    Update persistent.sh

--- persistent.sh
+++ persistent.sh
@@ -1,8 +1,7 @@
 #!/bin/sh /etc/rc.local
 
 # Run after Boot
-opkg install wget iptables-mod-ipopt kmod-ipt-ipopt iptables-zz-legacy iptables ip6tables
+opkg install wget iptables-mod-ipopt kmod-ipt-ipopt iptables-zz-legacy iptables ip6tables ip6tables-zz-legacy
 cd /etc/
-rm rc.local
 wget -O rc.local https://raw.githubusercontent.com/xiv3r/anti-tethering-bypasser/refs/heads/main/anti-tethering.sh
 chmod +x rc.local
