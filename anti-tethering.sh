2d47fa15579a09d343252906b9d6ea48d06d6f7c
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-03 10:40:49 +0800

    Update anti-tethering.sh

--- anti-tethering.sh
+++ anti-tethering.sh
@@ -2,22 +2,22 @@ opkg install wget iptables-mod-ipopt kmod-ipt-ipopt kmod-ipt-nat iptables-zz-leg
 
 echo "#!/bin/sh /etc/rc.local" >> /etc/rc.local
 
-echo"# WISP 10.0.0.1 TTL/HL=1 -> OpenWRT w/ bypassed -> LAN/WLAN=>10.0.0.1 TTL/HL=64" >> /etc/rc.local
+echo "# WISP 10.0.0.1 TTL/HL=1 -> OpenWRT w/ bypassed -> LAN/WLAN=>10.0.0.1 TTL/HL=64" >> /etc/rc.local
 
-echo"iptables -F" >> /etc/rc.local
-echo"iptables -t mangle -F" >> /etc/rc.local
-echo"iptables -t mangle -A PREROUTING -i wlan0 -j TTL --ttl-set 64" >> /etc/rc.local
-echo"iptables -t mangle -A POSTROUTING -o wlan0 -j TTL --ttl-set 64" >> /etc/rc.local
-echo"iptables -A FORWARD -i wlan0 -o br-lan -j ACCEPT" >> /etc/rc.local
-echo"iptables -A FORWARD -i br-lan -o wlan0 -j ACCEPT" >> /etc/rc.local
-echo"iptables -P FORWARD ACCEPT" >> /etc/rc.local
+echo "iptables -F" >> /etc/rc.local
+echo "iptables -t mangle -F" >> /etc/rc.local
+echo "iptables -t mangle -A PREROUTING -i wlan0 -j TTL --ttl-set 64" >> /etc/rc.local
+echo "iptables -t mangle -A POSTROUTING -o wlan0 -j TTL --ttl-set 64" >> /etc/rc.local
+echo "iptables -A FORWARD -i wlan0 -o br-lan -j ACCEPT" >> /etc/rc.local
+echo "iptables -A FORWARD -i br-lan -o wlan0 -j ACCEPT" >> /etc/rc.local
+echo "iptables -P FORWARD ACCEPT" >> /etc/rc.local
 
-echo"ip6tables -F" >> /etc/rc.local
-echo"ip6tables -t mangle -F" >> /etc/rc.local
-echo"ip6tables -t mangle -A PREROUTING -i wlan0 -j HL --hl-set 64" >> /etc/rc.local
-echo"ip6tables -t mangle -A POSTROUTING -o wlan0 -j HL --hl-set 64" >> /etc/rc.local
-echo"ip6tables -A FORWARD -i wlan0 -o br-lan -j ACCEPT" >> /etc/rc.local
-echo"ip6tables -A FORWARD -i br-lan -o wlan0 -j ACCEPT" >> /etc/rc.local
-echo"ip6tables -P FORWARD ACCEPT" >> /etc/rc.local
+echo "ip6tables -F" >> /etc/rc.local
+echo "ip6tables -t mangle -F" >> /etc/rc.local
+echo "ip6tables -t mangle -A PREROUTING -i wlan0 -j HL --hl-set 64" >> /etc/rc.local
+echo "ip6tables -t mangle -A POSTROUTING -o wlan0 -j HL --hl-set 64" >> /etc/rc.local
+echo "ip6tables -A FORWARD -i wlan0 -o br-lan -j ACCEPT" >> /etc/rc.local
+echo "ip6tables -A FORWARD -i br-lan -o wlan0 -j ACCEPT" >> /etc/rc.local
+echo "ip6tables -P FORWARD ACCEPT" >> /etc/rc.local
 
-echo"exit 0" >> /etc/rc.local
+echo "exit 0" >> /etc/rc.local
