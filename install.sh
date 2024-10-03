fa96a69c80a41d12a22647f7fcce68fa2422f326
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-03 09:03:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh 
 
-opkg update ; opkg install curl ; rm -rf /etc/rc.local ; curl https://raw.githubusercontent.com/xiv3r/anti-tethering-bypasser/refs/heads/main/persistent.sh | sh
+echo ' Installing Iptables/Ip6tables'
+rm -rf /etc/rc.local
+curl https://raw.githubusercontent.com/xiv3r/anti-tethering-bypasser/refs/heads/main/persistent.sh | sh
