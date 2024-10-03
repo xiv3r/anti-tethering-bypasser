d7beb19a1ac8688f4446b24cb23f99b01f655eb5
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-03 10:55:35 +0800

    Update anti-tethering.sh

--- anti-tethering.sh
+++ anti-tethering.sh
@@ -1,9 +1,7 @@
-opkg install wget iptables-mod-ipopt kmod-ipt-ipopt kmod-ipt-nat iptables-zz-legacy iptables ip6tables ip6tables-zz-legacy ip6tables-mod-nat kmod-ipt-nat6 kmod-ip6tables kmod-zram 
+opkg install wget iptables-mod-ipopt kmod-ipt-ipopt kmod-ipt-nat iptables-zz-legacy iptables ip6tables ip6tables-zz-legacy ip6tables-mod-nat kmod-ipt-nat6 kmod-ip6tables kmod-zram
 
 echo "#!/bin/sh /etc/rc.local" >> /etc/rc.local
-
 echo "# WISP 10.0.0.1 TTL/HL=1 -> OpenWRT w/ bypassed -> LAN/WLAN=>10.0.0.1 TTL/HL=64" >> /etc/rc.local
-
 echo "iptables -F" >> /etc/rc.local
 echo "iptables -t mangle -F" >> /etc/rc.local
 echo "iptables -t mangle -A PREROUTING -i wlan0 -j TTL --ttl-set 64" >> /etc/rc.local
@@ -21,3 +19,4 @@ echo "ip6tables -A FORWARD -i br-lan -o wlan0 -j ACCEPT" >> /etc/rc.local
 echo "ip6tables -P FORWARD ACCEPT" >> /etc/rc.local
 
 echo "exit 0" >> /etc/rc.local
+chmod +x /etc/rc.local
