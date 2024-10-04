3799c6690843e86cae3d13177ec0a0d6c27c2caa
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-04 17:53:02 +0800

    Update anti-tethering.sh

--- anti-tethering.sh
+++ anti-tethering.sh
@@ -1,15 +1,24 @@
-echo " Downloading Dependencies"
+echo "Downloading Dependencies"
 opkg install iptables-mod-ipopt kmod-ipt-ipopt kmod-ipt-nat iptables-zz-legacy iptables ip6tables ip6tables-zz-legacy ip6tables-mod-nat kmod-ipt-nat6 kmod-ip6tables kmod-zram
+
+echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sysctl -p
 
 echo "Installing iptables rule to /etc/rc.local"
 echo "#!/bin/sh /etc/rc.local" > /etc/rc.local
 echo "# WISP 10.0.0.1 TTL/HL=1 -> OpenWRT w/ bypassed -> LAN/WLAN=>10.0.0.1 TTL/HL=64" >> /etc/rc.local
+
 echo "iptables -F" >> /etc/rc.local
 echo "iptables -t mangle -F" >> /etc/rc.local
 echo "iptables -t mangle -A PREROUTING -i wlan0 -j TTL --ttl-set 64" >> /etc/rc.local
 echo "iptables -t mangle -A POSTROUTING -o wlan0 -j TTL --ttl-set 64" >> /etc/rc.local
 echo "iptables -A FORWARD -i wlan0 -o br-lan -j ACCEPT" >> /etc/rc.local
 echo "iptables -A FORWARD -i br-lan -o wlan0 -j ACCEPT" >> /etc/rc.local
+echo "iptables -A INPUT -i wlan0 -j ACCEPT" >> /etc/rc.local
+echo "iptables -A OUTPUT -o wlan0 -j ACCEPT" >> /etc/rc.local
+echo "iptables -A INPUT -i eth0 -j ACCEPT" >> /etc/rc.local
+echo "iptables -A OUTPUT -o eth0 -j ACCEPT" >> /etc/rc.local
 echo "iptables -P FORWARD ACCEPT" >> /etc/rc.local
 
 echo "Installing ip6tables rule to /etc/rc.local"
@@ -19,6 +28,10 @@ echo "ip6tables -t mangle -A PREROUTING -i wlan0 -j HL --hl-set 64" >> /etc/rc.l
 echo "ip6tables -t mangle -A POSTROUTING -o wlan0 -j HL --hl-set 64" >> /etc/rc.local
 echo "ip6tables -A FORWARD -i wlan0 -o br-lan -j ACCEPT" >> /etc/rc.local
 echo "ip6tables -A FORWARD -i br-lan -o wlan0 -j ACCEPT" >> /etc/rc.local
+echo "ip6tables -A INPUT -i wlan0 -j ACCEPT" >> /etc/rc.local
+echo "ip6tables -A OUTPUT -o wlan0 -j ACCEPT" >> /etc/rc.local
+echo "ip6tables -A INPUT -i eth0 -j ACCEPT" >> /etc/rc.local
+echo "ip6tables -A OUTPUT -o eth0 -j ACCEPT" >> /etc/rc.local
 echo "ip6tables -P FORWARD ACCEPT" >> /etc/rc.local
 
 echo "exit 0" >> /etc/rc.local
