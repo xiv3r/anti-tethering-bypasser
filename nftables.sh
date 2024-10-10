4b205e6cb5374b2e8e11bc6ad386a18d4569e6ab
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-10 17:49:07 +0800

    Update nftables.sh

--- nftables.sh
+++ nftables.sh
@@ -5,11 +5,14 @@ opkg install nftables kmod-nft-nat kmod-nft-core kmod-nft-nat kmod-nfnetlink
 echo "adding nftables to /etc/nftables.conf"
 
 echo "#!/bin/sh" > /etc/nftables.conf
-echo "nft add table inet custom_table" >> /etc/nftables.conf
 
-# Prerouting: Change TTL on incoming packets from wlan0
+# NFTABLE for IPv4 (recommended)
+# ______________________________
+
+echo "nft add table inet custom_table" >> /etc/nftables.conf
+# Prerouting: Change TTL=1 to TTL=64 on incoming packets from wlan0
 echo "nft add chain inet custom_table prerouting { type filter hook prerouting priority 0 \; }" >> /etc/nftables.conf
-echo "nft add rule inet custom_table prerouting iif "wlan0" ip ttl set 64" >> /etc/nftables.conf
+echo "nft add rule inet custom_table prerouting iif "wlan0" ip ttl set 65" >> /etc/nftables.conf
 
 # Postrouting: Enable masquerading on eth0 and set outgoing TTL for wlan0
 echo "nft add chain inet custom_table postrouting { type nat hook postrouting priority 100 \; }" >> /etc/nftables.conf
@@ -22,12 +25,14 @@ echo "nft add rule inet custom_table forward iif "wlan0" oif "eth0" accept" >> /
 echo "nft add rule inet custom_table forward iif "eth0" oif "wlan0" accept" >> /etc/nftables.conf
 
 # NFTABLE for IPv6 (optional)
+# ___________________________
+
 # Prerouting: Change HL=1 to HL=64 on incoming packets from wlan0
-echo "nft add chain inet custom_table prerouting { type filter hook prerouting priority 0 \; }" >> /etc/nftables.conf
-echo "nft add rule inet custom_table prerouting iif "wlan0" ip6 hl set 64" >> /etc/nftables.conf
+# nft add chain inet custom_table prerouting { type filter hook prerouting priority 0 \; }
+echo "nft add rule inet custom_table prerouting iif "wlan0" ip6 hl set 65" >> /etc/nftables.conf
 
 # Postrouting: Enable masquerading on eth0 and set outgoing HL for wlan0
-echo "nft add chain inet custom_table postrouting { type nat hook postrouting priority 100 \; }" >> /etc/nftables.conf
+# nft add chain inet custom_table postrouting { type nat hook postrouting priority 100 \; }
 # nft add rule inet custom_table postrouting oif "eth0" masquerade
 echo "nft add rule inet custom_table postrouting oif "wlan0" ip6 hl set 64" >> /etc/nftables.conf
 
