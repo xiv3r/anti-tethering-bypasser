ebf1687e7691695fafb15bfccd4399b8769d5aff
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-16 08:59:59 +0800

    Update nftables.sh

--- nftables.sh
+++ nftables.sh
@@ -25,23 +25,6 @@ echo "nft add chain inet custom_table forward { type filter hook forward priorit
 echo "nft add rule inet custom_table forward iif "wlan0" oif "eth0" accept" >> /etc/nftables.conf
 echo "nft add rule inet custom_table forward iif "eth0" oif "wlan0" accept" >> /etc/nftables.conf
 
-# NFTABLE for IPv6 (optional)
-# ___________________________
-
-# Prerouting: Change HL=1 to HL=64 on incoming packets from wlan0
-# nft add chain inet custom_table prerouting { type filter hook prerouting priority 0 \; }
-# echo "nft add rule inet custom_table prerouting iif "wlan0" ip6 hl set 65" >> /etc/nftables.conf
-
-# Postrouting: Enable masquerading on eth0 and set outgoing HL for wlan0
-# nft add chain inet custom_table postrouting { type nat hook postrouting priority 100 \; }
-# nft add rule inet custom_table postrouting oif "eth0" masquerade
-# echo "nft add rule inet custom_table postrouting oif "wlan0" ip6 hl set 64" >> /etc/nftables.conf
-
-# Forwarding: Allow traffic between wlan0 and eth0 in both directions
-# nft add chain inet custom_table forward { type filter hook forward priority 0 \; }
-# nft add rule inet custom_table forward iif "wlan0" oif "eth0" accept
-# nft add rule inet custom_table forward iif "eth0" oif "wlan0" accept
-
 chmod +x /etc/nftables.conf
 sh /etc/nftables.conf
 
