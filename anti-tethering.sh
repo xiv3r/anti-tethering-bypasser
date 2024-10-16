091d4ce53e6468f427363408ef3b706e955499c4
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-16 08:58:03 +0800

    Update anti-tethering.sh

--- anti-tethering.sh
+++ anti-tethering.sh
@@ -12,19 +12,16 @@ echo "# WISP 10.0.0.1 TTL/HL=1 -> OpenWRT w/ bypassed -> LAN/WLAN=>10.0.0.1 TTL/
 
 echo "iptables -F" >> /etc/rc.local
 echo "iptables -t mangle -F" >> /etc/rc.local
+echo "iptables -t nat -F" >> /etc/rc.local
+echo "ip6tables -F" >> /etc/rc.local
+echo "ip6tables -t mangle -F" >> /etc/rc.local
 echo "iptables -t mangle -A PREROUTING -i wlan0 -j TTL --ttl-set 65" >> /etc/rc.local
 echo "iptables -t mangle -A POSTROUTING -o wlan0 -j TTL --ttl-set 64" >> /etc/rc.local
 echo "iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE" >> /etc/rc.local
-echo "iptables -A FORWARD -i wlan0 -o br-lan -j ACCEPT" >> /etc/rc.local
-echo "iptables -A FORWARD -i br-lan -o wlan0 -j ACCEPT" >> /etc/rc.local
-
-echo "Installing ip6tables rule to /etc/rc.local"
-echo "ip6tables -F" >> /etc/rc.local
-echo "ip6tables -t mangle -F" >> /etc/rc.local
 echo "ip6tables -t mangle -A PREROUTING -i wlan0 -j HL --hl-set 65" >> /etc/rc.local
 echo "ip6tables -t mangle -A POSTROUTING -o wlan0 -j HL --hl-set 64" >> /etc/rc.local
-echo "ip6tables -A FORWARD -i wlan0 -o br-lan -j ACCEPT" >> /etc/rc.local
-echo "ip6tables -A FORWARD -i br-lan -o wlan0 -j ACCEPT" >> /etc/rc.local
+echo "iptables -A FORWARD -i wlan0 -o br-lan -j ACCEPT" >> /etc/rc.local
+echo "iptables -A FORWARD -i br-lan -o wlan0 -j ACCEPT" >> /etc/rc.local
 
 echo "exit 0" >> /etc/rc.local
 chmod +x /etc/rc.local
