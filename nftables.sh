a8d61929c8f394bf1ccb487ee93519f0d5679bf8
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-31 17:01:16 +0800

    Update nftables.sh

--- nftables.sh
+++ nftables.sh
@@ -1,33 +1,11 @@
-#!/bin/sh
+chain mangle_prerouting_ttl65 {
+  type filter hook prerouting priority 300; policy accept;
+  iifname "wlan0" counter ip ttl set 65
+  iifname "wlan0" counter ip6 hoplimit set 65
+}
 
-echo "Downloading dependencies"
-opkg install nftables kmod-nft-nat kmod-nft-core kmod-nfnetlink
-
-echo "adding nftables to /etc/nftables.conf"
-
-echo "#!/bin/sh" > /etc/nftables.conf
-
-# NFTABLE for IPv4 (recommended)
-# ______________________________
-
-echo "nft add table inet custom_table" >> /etc/nftables.conf
-# Prerouting: Change TTL=1 to TTL=64 on incoming packets from wlan0
-echo "nft add chain inet custom_table prerouting { type filter hook prerouting priority 0 \; }" >> /etc/nftables.conf
-echo "nft add rule inet custom_table prerouting iif "wlan0" ip ttl set 65" >> /etc/nftables.conf
-
-# Postrouting: Enable masquerading on eth0 and set outgoing TTL for wlan0
-echo "nft add chain inet custom_table postrouting { type nat hook postrouting priority 100 \; }" >> /etc/nftables.conf
-echo "nft add rule inet custom_table postrouting oif "eth0" masquerade" >> /etc/nftables.conf
-echo "nft add rule inet custom_table postrouting oif "wlan0" ip ttl set 64" >> /etc/nftables.conf
-
-# Forwarding: Allow traffic between wlan0 and eth0 in both directions
-echo "nft add chain inet custom_table forward { type filter hook forward priority 0 \; }" >> /etc/nftables.conf
-echo "nft add rule inet custom_table forward iif "wlan0" oif "eth0" accept" >> /etc/nftables.conf
-echo "nft add rule inet custom_table forward iif "eth0" oif "wlan0" accept" >> /etc/nftables.conf
-
-chmod +x /etc/nftables.conf
-sh /etc/nftables.conf
-
-echo "Done installing config into /etc/nftables.conf"
-echo "nftables is running now on wlan0 to eth0 with ttl=64"
-nftables list ruleset
+chain mangle_postrouting_ttl65 {
+  type filter hook postrouting priority 300; policy accept;
+  oifname "wlan0" counter ip ttl set 65
+  oifname "wlan0" counter ip6 hoplimit set 65
+}
