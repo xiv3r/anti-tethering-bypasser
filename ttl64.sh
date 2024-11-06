9c7087e5355f63b71795686a3d7c9669cd2db82a
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-06 16:12:36 +0800

    Update and rename ttl64.nft to ttl64.sh

--- ttl64.sh
+++ ttl64.sh
@@ -1,5 +1,11 @@
+#!/bin/sh
+
+echo "
 chain mangle_prerouting_ttl64 {
   type filter hook prerouting priority 300; policy accept;
   counter ip ttl set 64
   counter ip6 hoplimit set 64
 }
+" > /etc/nftables.d/ttl64.nft
+fw4 check
+/etc/init.d/firewall restart
