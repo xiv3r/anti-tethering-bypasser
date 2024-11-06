eaf0faf237fd7c9abcf52065321cbfaaa71dba5b
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-06 23:33:21 +0800

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 echo "Downloading Dependencies"
 opkg install iptables-mod-ipopt iptables-zz-legacy ip6tables-zz-legacy
 
@@ -5,10 +7,12 @@ echo "
 net.ipv6.conf.all.forwarding=1
 net.ipv4.ip_forward=1
 " >> /etc/sysctl.conf
+###
 sysctl -p
 
 echo "Installing iptables rule to /etc/rc.local"
 sed -i 's/exit 0//' /etc/rc.local
+###
 echo "
 #!/bin/sh /etc/rc.local
 iptables -t mangle -A PREROUTING -j TTL --ttl-set 64
@@ -18,8 +22,12 @@ iptables -A FORWARD -i wlan0 -o br-lan -j ACCEPT
 iptables -A FORWARD -i br-lan -o wlan0 -j ACCEPT
 exit 0
 " >> /etc/rc.local
+##$
 chmod +x /etc/rc.local
+###
 sh /etc/rc.local
 echo "Done Installing"
+###
 iptables -vnL --line-numbers
+###
 ip6tables -vnL --line-numbers
