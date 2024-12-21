baf8c91d6a42a33759bcf9f6e844cc2241b889de
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-12-21 11:28:22 +0800

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -5,7 +5,6 @@ opkg install iptables-mod-ipopt iptables-zz-legacy ip6tables-zz-legacy
 wget -O /etc/sysctl.conf https://raw.githubusercontent.com/xiv3r/anti-tethering-bypasser/refs/heads/main/sysctl
 ###
 sysctl -p
-# sed -i 's/exit 0//' /etc/rc.local
 ###
 cat >/etc/iptables/rules.v4 << EOF
 iptables -t mangle -A PREROUTING -j TTL --ttl-set 64
