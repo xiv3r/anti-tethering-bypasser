2e5e91328b467b8ee0e3561b1bee3b55e14846cd
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-12-21 11:25:28 +0800

    Update ttl64.sh

--- ttl64.sh
+++ ttl64.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-echo "
+
+cat >/etc/nftables.d/ttl64.nft << EOF
 chain mangle_prerouting_ttl64 {
   type filter hook prerouting priority 300; policy accept;
   counter ip ttl set 64
   counter ip6 hoplimit set 64
 }
-" > /etc/nftables.d/ttl64.nft
+EOF
 ###
-fw4 check && /etc/init.d/firewall restart
+fw4 check
+###
+/etc/init.d/firewall restart
