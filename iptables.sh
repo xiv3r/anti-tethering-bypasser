46ecbf9d482d3b263fdd16dd3068eb38f40e0128
xiv3r <117867334+xiv3r@users.noreply.github.com>
2025-03-29 09:33:47 +0800

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 opkg install iptables-mod-ipopt iptables-zz-legacy ip6tables-zz-legacy
 ###
 cat >/etc/iptables/rules.v4 << EOF
 iptables -t mangle -A PREROUTING -j TTL --ttl-set 64
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 ip6tables -t mangle -A PREROUTING -j HL --hl-set 64
 EOF
 ###
