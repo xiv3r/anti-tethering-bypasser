1c33951cfc8c41809ea8ac778ddd021c8c46b044
xiv3r <117867334+xiv3r@users.noreply.github.com>
2025-03-29 11:37:40 +0800

    Update ttl64.sh

--- ttl64.sh
+++ ttl64.sh
@@ -1,5 +1,11 @@
 chain mangle_prerouting_ttl64 {
-   type filter hook prerouting priority 300; policy accept;
-   ip ttl set 64
-   ip6 hoplimit set 64
-}
+                type filter hook prerouting priority 300; policy accept;
+                ip ttl set 64
+                ip6 hoplimit set 64
+        }
+
+chain mangle_postrouting_ttl64 {
+                type filter hook postrouting priority 300; policy accept;
+                ip ttl set 64
+                ip6 hoplimit set 64
+        }
