fdbdf88c26ce9b333bfbbe3c1f4705874eb6f6f0
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-07-16 19:30:36 +0800

    Update ttl64.sh

--- ttl64.sh
+++ ttl64.sh
@@ -3,9 +3,3 @@ chain mangle_prerouting_ttl64 {
                 ip ttl set 64
                 ip6 hoplimit set 64
         }
-
-chain mangle_postrouting_ttl64 {
-                type filter hook postrouting priority 300; policy accept;
-                ip ttl set 64
-                ip6 hoplimit set 64
-        }
