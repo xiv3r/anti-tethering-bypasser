a076e3d5a09a4a1bcfcfe2c2fd158b9faab7692b
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-10-03 05:56:26 +0800

    Fixed

--- bypass-captive-portal.sh
+++ bypass-captive-portal.sh
@@ -9,7 +9,3 @@ iptables -t mangle -I PREROUTING 1 -s "$IP" -j MARK --set-xmark 0x13/0xffffffff
 iptables -t mangle -I PREROUTING 2 -s "$IP" -j RETURN
 iptables -I FORWARD 1 -m mac --mac-source "$MAC" -j ACCEPT
 iptables -I FORWARD 2 -d "$IP" -j ACCEPT
-
-# TTL adjust bypass 
-iptables -t mangle -I POSTROUTING 1 -s "$IP" -o eth0.22 -j TTL --ttl-set 64
-iptables -t mangle -I POSTROUTING 1 -d "$IP" -o eth0.22 -j TTL --ttl-set 64
