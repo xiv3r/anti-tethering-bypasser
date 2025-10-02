9b65227078a5e1107a9f0baf4bb4e1421ad7c465
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-10-02 10:34:47 +0800

    bypass captive portal

---
+++ bypass-captive-portal.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Captive portal bypass 
+IP="10.0.15.100"
+MAC="32:0C:43:01:76:80"
+
+# Captive portal bypass rules
+iptables -t mangle -I PREROUTING 1 -s "$IP" -j MARK --set-xmark 0x13/0xffffffff
+iptables -t mangle -I PREROUTING 2 -s "$IP" -j RETURN
+iptables -I FORWARD 1 -m mac --mac-source "$MAC" -j ACCEPT
+iptables -I FORWARD 2 -d "$IP" -j ACCEPT
+
+# TTL adjust
+iptables -t mangle -I PREROUTING 1 -s "$IP" -j TTL --ttl-set 64
+iptables -t mangle -I POSTROUTING 1 -d "$IP" -j TTL --ttl-set 64
